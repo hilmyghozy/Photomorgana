e034586c06a9ef5f9d646dd619726ed03f3408ba
hilmyghozy <hilmyghozy11@gmail.com>
2020-05-11 17:45:44 +0700

    Initial Commit

---
+++ Photomorgana/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Photomorgana
+//
+//  Created by hilmy ghozy on 11/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
