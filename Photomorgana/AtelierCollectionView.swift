9389399a0653033e4afa039cc33b1ff6b2da9cab
hilmyghozy <hilmyghozy11@gmail.com>
2020-05-19 18:30:11 +0700

    echo "# Photomorgana" >> README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/hilmyghozy/Photomorgana.git git push -u origin master

---
+++ Photomorgana/AtelierCollectionView.swift
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.swift
+//  Photomorgana
+//
+//  Created by hilmy ghozy on 11/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+
+
+class AtelierCollectionView: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UICollectionViewDelegateFlowLayout {
+    
+    var images = [UIImage]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Atelier"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPhoto))
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        let imageView = cell.viewWithTag(1000) as? UIImageView
+        imageView?.image = images[indexPath.item]
+        
+        return cell
+    }
+    
+    
+    @objc func importPhoto(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true, completion: nil)
+        
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 3 - 1
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+
+}
+
