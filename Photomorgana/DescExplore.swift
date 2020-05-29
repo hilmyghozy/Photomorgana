257155baf91c1067bda362b306b82e0eb6029404
NaniSukmaP <luckyputri98@gmail.com>
2020-05-29 13:41:48 +0700

    add asset

---
+++ Photomorgana/DescExplore.swift
@@ -0,0 +1,14 @@
+//
+//  DescExplore.swift
+//  Photomorgana
+//
+//  Created by Nani Sukma Putri Pratama on 28/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+var ExploreDesc : [Explore] = [
+    Explore(profileExp: "Name", imgProfileExp: UIImage(named: "city")!, imgExp: UIImage(named: "city")!, likeExp: "20", commentExp: "20", tagExp: "city"),
+Explore(profileExp: "Name", imgProfileExp: UIImage(named: "city")!, imgExp: UIImage(named: "city")!, likeExp: "20", commentExp: "20", tagExp: "city")
+    
+]
---
+++ Photomorgana/Explore.swift
@@ -0,0 +1,17 @@
+//
+//  Explore.swift
+//  Photomorgana
+//
+//  Created by Nani Sukma Putri Pratama on 28/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+struct Explore {
+    let profileExp : String
+    let imgProfileExp : UIImage
+    let imgExp : UIImage
+    let likeExp : String
+    let commentExp : String
+    let tagExp : String
+}
