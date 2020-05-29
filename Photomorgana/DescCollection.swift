7bbde42036c833fb198364b926a489f1f628d2d1
NaniSukmaP <luckyputri98@gmail.com>
2020-05-29 15:38:10 +0700

    add file

---
+++ Photomorgana/DescCollection.swift
@@ -0,0 +1,13 @@
+//
+//  DescCollection.swift
+//  Photomorgana
+//
+//  Created by Nani Sukma Putri Pratama on 27/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+var Collect : [Collection] = [
+Collection(comment: "12", like: "18", tag: "City", caption: "Testing caption for this apps , I hope this can run well without big bug", imageCollection: UIImage(named: "city")!),
+    Collection(comment: "12", like: "18", tag: "City", caption: "Testing caption for this apps , I hope this can run well without big bug", imageCollection: UIImage(named: "city")!)
+]
---
+++ Photomorgana/ProfileTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileTableViewCell.swift
+//  Photomorgana
+//
+//  Created by Nani Sukma Putri Pratama on 27/05/20.
+//  Copyright © 2020 hilmy ghozy. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    @IBOutlet weak var imageCollection: UIImageView!
+    @IBOutlet weak var captionCollection: UILabel!
+    @IBOutlet weak var likeCollection: UILabel!
+    @IBOutlet weak var commentCollection: UILabel!
+    @IBOutlet weak var tagCollection: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
